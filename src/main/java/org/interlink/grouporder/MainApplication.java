0f929799b76f90dbb3783320f5d8c0dba37e59a1
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-14 13:03:51 +0200

    'Initial FirstCommit'

---
+++ src/main/java/org/interlink/grouporder/MainApplication.java
@@ -0,0 +1,16 @@
+package org.interlink.grouporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        final SpringApplication application = new SpringApplication(MainApplication.class);
+        application.addListeners(new ApplicationPidFileWriter("./app.pid"));
+        application.run(args);
+    }
+}
