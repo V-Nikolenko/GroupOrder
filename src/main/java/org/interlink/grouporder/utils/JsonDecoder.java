5fb951527b0508792d17f4bc29c33412e4d0ecb0
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-16 18:17:43 +0200

    #25 JsonDecoder: enhance: Removed some unused fields in JsonDecoder.java

--- src/main/java/org/interlink/grouporder/utils/JsonDecoder.java
+++ src/main/java/org/interlink/grouporder/utils/JsonDecoder.java
@@ -1,9 +1,6 @@
 package org.interlink.grouporder.utils;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import org.interlink.grouporder.entity.Product;
 
 import java.math.BigDecimal;
@@ -12,12 +9,14 @@ import java.util.List;
 
 public class JsonDecoder {
     private JsonObject jsonObject;
-
+    private List<Product> memberOrder;
     private BigDecimal fullPrice;
 
     public JsonDecoder(String json) {
         jsonObject = new Gson().fromJson(json, JsonObject.class);
-        this.fullPrice = new BigDecimal(jsonObject.get("fullPrice").getAsString());
+
+        this.fullPrice = new BigDecimal(jsonObject.get("fullPrice").toString());
+        this.memberOrder = rebaseJsonToProduct();
     }
 
     public BigDecimal getOrderPrice() {
@@ -26,28 +25,21 @@ public class JsonDecoder {
 
     public List<Product> rebaseJsonToProduct() {
         JsonArray items = jsonObject.getAsJsonArray("items");
-        List<Product> memberOrder = new LinkedList<>();
+        memberOrder = new LinkedList<>();
 
         for (JsonElement element : items) {
             JsonObject object = element.getAsJsonObject();
 
-            String id = object.get("id").getAsString();
-            String name = object.get("name").getAsString();
-            String price = object.get("price").getAsString();
-            String image = object.get("image").getAsString();
-            String count = object.get("count").getAsString();
-            String key = object.get("key").getAsString();
-            String optionId = object.get("optionId").getAsString();
+            int id = object.get("id").getAsInt();
+            int optionId = object.get("optionId").getAsInt();
             String optionValue = object.get("optionValue").getAsString();
-            String priceExpirationTime = object.get("priceExpirationTime").getAsString();
             String measure = object.get("measure").getAsString();
             String measureType = object.get("measureType").getAsString();
-            String standardPrice = object.get("standardPrice").getAsString();
-            String packagePrice = object.get("packagePrice").getAsString();
-            String maxCountPositionInPackage = object.get("maxCountPositionInPackage").getAsString();
+            JsonNull packagePrice = object.get("packagePrice").getAsJsonNull();
+            int maxCountPositionInPackage = object.get("maxCountPositionInPackage").getAsInt();
 
-            Product product = new Product(id, name, price, image, count, key, optionId, optionValue,
-                    priceExpirationTime, measure, measureType, standardPrice, packagePrice, maxCountPositionInPackage);
+            Product product = new Product(id, optionId, optionValue, measure, measureType, packagePrice,
+                    maxCountPositionInPackage);
 
             memberOrder.add(product);
         }
