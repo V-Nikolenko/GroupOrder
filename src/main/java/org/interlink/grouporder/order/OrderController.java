3a26257252c852284e5f1b0dabb6f8d26ddb49ad
Andrew Maslyuk <tenza1999@gmail.com>
2020-01-17 15:31:00 +0200

    #49_issue createNewOrder: add: add returning of order code

--- src/main/java/org/interlink/grouporder/order/OrderController.java
+++ src/main/java/org/interlink/grouporder/order/OrderController.java
@@ -1,32 +1,28 @@
 package org.interlink.grouporder.order;
 
 
-import com.google.gson.JsonNull;
 import org.interlink.grouporder.entity.Order;
-import org.interlink.grouporder.entity.Product;
-import org.interlink.grouporder.utils.JsonEncoder;
 import org.interlink.grouporder.utils.OrderCodeGenerator;
 import org.interlink.grouporder.utils.OrderStorage;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/orders")
 public class OrderController {
-    private String key;
 
     @PostMapping
-    public void createNewOrder(@RequestBody String json) {
+    public String createNewOrder(@RequestBody String json) {
         Order order = new Order();
         OrderStorage orderStorage = new OrderStorage();
         OrderCodeGenerator orderCodeGenerator = new OrderCodeGenerator();
 
-        key = orderCodeGenerator.generateOrderCode();
+        String key = orderCodeGenerator.generateOrderCode();
 
         orderStorage.addOrder(key, order);
-    }
 
-    @GetMapping
-    public String sendOrderCode() {
         return key;
     }
 }
