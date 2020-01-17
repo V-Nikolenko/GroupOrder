e19188479b29037c256ac3a0b2653623b015594b
Vitaliy5 <dydvitrom@ukr.net>
2020-01-17 11:21:41 +0200

    #37_issue front-end: add: fixed bug installing extesion

--- Extention/popUp.js
+++ Extention/popUp.js
@@ -1,6 +1,5 @@
 btn1.addEventListener('click', () => {
     let name = newOrdinp.value;
-    // alert(name);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {type: "getOrders"}, function(response){
             response.name = name;
