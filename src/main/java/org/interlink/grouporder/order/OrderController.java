fbefff3a6eac128e37bf5a1f5b47714484c2a8d9
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-17 12:43:18 +0200

    #42 OrderController: enhance: Added return statement to the extension

--- src/main/java/org/interlink/grouporder/order/OrderController.java
+++ src/main/java/org/interlink/grouporder/order/OrderController.java
@@ -1,14 +1,30 @@
 package org.interlink.grouporder.order;
 
 
+import org.interlink.grouporder.entity.Order;
+import org.interlink.grouporder.utils.OrderCodeGenerator;
+import org.interlink.grouporder.utils.OrderStorage;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/orders")
 public class OrderController {
+    private String key;
 
     @PostMapping
-    public void createNewOrder(@RequestBody String json){
-        System.out.println(json);
+    public void createNewOrder(@RequestBody String json) {
+        Order order = new Order();
+        OrderStorage orderStorage = new OrderStorage();
+        OrderCodeGenerator orderCodeGenerator = new OrderCodeGenerator();
+
+        key = orderCodeGenerator.generateOrderCode();
+
+        orderStorage.addOrder(key, order);
+    }
+
+    @GetMapping
+    public String sendOrderCode() {
+        return key;
     }
 }
+
