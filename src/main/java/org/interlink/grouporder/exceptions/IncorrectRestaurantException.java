8559009b2725f01cc8009ce6f78b7a1616dae700
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-20 16:31:52 +0200

    #76 IncorrectRestaurantException: feat: When user choose incorrect restaurant - he get warning

---
+++ src/main/java/org/interlink/grouporder/exceptions/IncorrectRestaurantException.java
@@ -0,0 +1,7 @@
+package org.interlink.grouporder.exceptions;
+
+public class IncorrectRestaurantException extends RuntimeException {
+    public IncorrectRestaurantException(String message) {
+        super(message);
+    }
+}
