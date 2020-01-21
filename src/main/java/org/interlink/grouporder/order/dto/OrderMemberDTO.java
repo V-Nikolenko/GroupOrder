c7ef75dbc300939e5d28cd03f63e897cfee24bb8
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-21 16:46:18 +0200

    #106 OrderMemberDTO: fix: refactor some OrderMemberDTO fields

--- src/main/java/org/interlink/grouporder/order/dto/OrderMemberDTO.java
+++ src/main/java/org/interlink/grouporder/order/dto/OrderMemberDTO.java
@@ -2,16 +2,13 @@ package org.interlink.grouporder.order.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.interlink.grouporder.entity.Product;
+import org.interlink.grouporder.entity.OrderMember;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
 @Setter
 public class OrderMemberDTO {
 
-    private String name;
-    private BigDecimal fullPrice;
-    private List<Product> items;
+    private List<OrderMember> members;
 }
