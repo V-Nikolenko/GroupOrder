a86e9f89831b2fc092d5e4b3f10056f22de8f6d7
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-21 16:44:18 +0200

    #106 OrderDTO: fix: remove some OrderDTO fields

--- src/main/java/org/interlink/grouporder/order/dto/OrderDTO.java
+++ src/main/java/org/interlink/grouporder/order/dto/OrderDTO.java
@@ -2,25 +2,18 @@ package org.interlink.grouporder.order.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.interlink.grouporder.entity.Product;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
 public class OrderDTO {
 
-    //User information
     private String name;
     private BigDecimal fullPrice;
     private int companyId;
 
-    // Product information
-    private int dishId;
-    private int optionId;
-    private String optionValue;
-    private String measure;
-    private String measureType;
-    private String packagePrice;
-    private int maxCountPositionInPackage;
-
+    private List<Product> items;
 }
