d5d6c74a51979eb620a4e584879b45fe0a0d3e44
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-22 14:35:30 +0200

    #111 DataStorage: enhance: Create order storage for GroupOrder's objects

---
+++ src/main/java/org/interlink/grouporder/core/data/DataStorage.java
@@ -0,0 +1,29 @@
+package org.interlink.grouporder.core.data;
+
+import org.interlink.grouporder.core.entity.GroupOrder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DataStorage {
+
+    private static Map<String, GroupOrder> orders = new LinkedHashMap<>();
+
+    public static void addGroupOrder(String code, GroupOrder groupOrder) {
+        orders.put(code, groupOrder);
+    }
+
+    public static void removeGroupOrder(String code) {
+        orders.remove(code);
+    }
+
+    public static GroupOrder getGroupOrder(String code) {
+        return Optional.ofNullable(orders.get(code)).orElseThrow(
+                () -> new IllegalArgumentException("No such key in the database [" + code + "]"));
+    }
+
+    public static boolean isContains(String code) {
+        return orders.containsKey(code);
+    }
+}
