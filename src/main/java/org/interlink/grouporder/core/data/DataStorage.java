cc8ad199dffccc7051ecb2dbc2c0cdd522457fad
AndrewThief <tenza1999@gmail.com>
2020-01-23 11:13:48 +0200

    #67_issue addMemberToOrder: add: added new method to controller

--- src/main/java/org/interlink/grouporder/core/data/DataStorage.java
+++ src/main/java/org/interlink/grouporder/core/data/DataStorage.java
@@ -1,6 +1,7 @@
 package org.interlink.grouporder.core.data;
 
 import org.interlink.grouporder.core.entity.GroupOrder;
+import org.interlink.grouporder.core.entity.MemberOrder;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -21,6 +22,15 @@ public class DataStorage {
     public static GroupOrder getGroupOrder(String code) {
         return Optional.ofNullable(orders.get(code)).orElseThrow(
                 () -> new IllegalArgumentException("No such key in the database [" + code + "]"));
+    }
+
+    public static void addMemberToOrder(String code, MemberOrder memberOrder) {
+        if (orders.containsKey(code) && memberOrder != null) {
+            GroupOrder groupOrder = orders.get(code);
+            groupOrder.addMemberToGroupOrder(memberOrder);
+
+            orders.replace(code, groupOrder);
+        }
     }
 
     public static boolean isContains(String code) {
--- src/main/java/org/interlink/grouporder/misteram/controllers/OrderController.java
+++ src/main/java/org/interlink/grouporder/misteram/controllers/OrderController.java
@@ -4,8 +4,10 @@ package org.interlink.grouporder.misteram.controllers;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.interlink.grouporder.core.data.DataStorage;
 import org.interlink.grouporder.core.entity.GroupOrder;
+import org.interlink.grouporder.core.entity.MemberOrder;
 import org.interlink.grouporder.core.entity.view.GroupOrderView;
 import org.interlink.grouporder.core.utils.OrderCodeGenerator;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +27,10 @@ public class OrderController {
         return groupOrder;
     }
 
+    @JsonView(GroupOrderView.Extended.class)
+    @PostMapping("/{code}/add-order")
+    public GroupOrder addMemberToOrder(@PathVariable("code") String code, MemberOrder memberOrder) {
+        DataStorage.addMemberToOrder(code, memberOrder);
+        return DataStorage.getGroupOrder(code);
+    }
 }
