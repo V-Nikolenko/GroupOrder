1bf90b386d066b5c2b3838f29e1123082957a1dd
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-23 16:34:12 +0200

    #134_issue MisterAmMapper: enhance: Added support for any types of parameter.

--- src/main/java/org/interlink/grouporder/misteram/MisterAmMapper.java
+++ src/main/java/org/interlink/grouporder/misteram/MisterAmMapper.java
@@ -1,8 +1,12 @@
 package org.interlink.grouporder.misteram;
 
+import org.interlink.grouporder.core.entity.MemberOrder;
+import org.interlink.grouporder.core.entity.Product;
+import org.interlink.grouporder.misteram.entity.Order;
 import org.modelmapper.ModelMapper;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MisterAmMapper {
 
@@ -12,4 +16,12 @@ public class MisterAmMapper {
         return mapper.map(object, objectClass);
     }
 
+    public static MemberOrder map(Order source, MemberOrder destination) {
+        List<Product> products = source.getItems().stream().map(item -> map(item, Product.class)).collect(Collectors.toList());
+        destination.setName(source.getName());
+        destination.setProducts(products);
+        destination.setUrl(source.getUrl());
+
+        return destination;
+    }
 }
