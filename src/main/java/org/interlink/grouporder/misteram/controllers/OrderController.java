0969f69f32132963a006b335355912bda1934511
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-24 10:14:34 +0200

    #134_issue MisterAmMapper: fix: Restored deleted code.

--- src/main/java/org/interlink/grouporder/misteram/controllers/OrderController.java
+++ src/main/java/org/interlink/grouporder/misteram/controllers/OrderController.java
@@ -28,19 +28,6 @@ public class OrderController {
         return groupOrder;
     }
 
-    @JsonView(GroupOrderView.Basic.class)
-    @PostMapping("/{code}/add-order")
-    public ResponseEntity addMemberToOrder(@PathVariable("code") String code, @RequestBody Order newOrder) {
-        try {
-            MemberOrder memberOrder = MisterAmMapper.map(newOrder, MemberOrder.class);
-
-            DataStorage.getGroupOrder(code).addMemberToGroupOrder(memberOrder);
-            return ResponseEntity.ok("Success");
-        }catch (Exception e) {
-            return ExceptionsHandler.handleException(e);
-        }
-    }
-
     @PostMapping("{code}/connect")
     public ResponseEntity connectToGroupOrder(@PathVariable("code") String code) {
         try {
@@ -54,7 +41,20 @@ public class OrderController {
         }
     }
 
-    @GetMapping("{code}/show-group-order")
+    @JsonView(GroupOrderView.Basic.class)
+    @PostMapping("/{code}/add-order")
+    public ResponseEntity addMemberToOrder(@PathVariable("code") String code, @RequestBody Order newOrder) {
+        try {
+            MemberOrder memberOrder = MisterAmMapper.map(newOrder, new MemberOrder());
+
+            DataStorage.getGroupOrder(code).addMemberToGroupOrder(memberOrder);
+            return ResponseEntity.ok("Success");
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
+
+    @GetMapping("/{code}/show-group-order")
     public ResponseEntity showGroupOrder(@PathVariable("code") String code) {
         try {
             GroupOrder groupOrder = DataStorage.getGroupOrder(code);
@@ -64,4 +64,13 @@ public class OrderController {
         }
     }
 
+    @GetMapping("/{code}/form-group-order")
+    public ResponseEntity formGroupOrder(@PathVariable("code") String code) {
+        try {
+            GroupOrder groupOrder = DataStorage.getGroupOrder(code);
+            return ResponseEntity.ok(MisterAmMapper.map(groupOrder, new Order()));
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
 }
