cca46d9afe83cc3d64ae52ef598d3dfaaa0744ce
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-28 10:03:17 +0200

    #134_issue FullOrderItemsDTO: enhance: Return list of items

---
+++ src/main/java/org/interlink/grouporder/misteram/MisterAmMapper.java
@@ -0,0 +1,48 @@
+package org.interlink.grouporder.misteram;
+
+import org.interlink.grouporder.core.entity.GroupOrder;
+import org.interlink.grouporder.core.entity.MemberOrder;
+import org.interlink.grouporder.core.entity.Product;
+import org.interlink.grouporder.misteram.entity.FullOrderItemsDTO;
+import org.interlink.grouporder.misteram.entity.ItemDTO;
+import org.interlink.grouporder.misteram.entity.MemberOrderDTO;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MisterAmMapper {
+
+    private MisterAmMapper() {
+    }
+
+    private static final ModelMapper mapper = new ModelMapper();
+
+    public static <T> T map(Object object, Class<T> objectClass) {
+        return mapper.map(object, objectClass);
+    }
+
+    public static MemberOrder map(MemberOrderDTO source, MemberOrder destination) {
+        List<Product> products = source.getItems().stream()
+                .map(item -> map(item, Product.class))
+                .collect(Collectors.toList());
+
+        destination.setName(source.getName());
+        destination.setProducts(products);
+        destination.setUrl(source.getUrl());
+        destination.setFullPrice(source.getFullPrice());
+
+        return destination;
+    }
+
+    public static FullOrderItemsDTO map(GroupOrder source, FullOrderItemsDTO destination) {
+        List<ItemDTO> items = source.getAllProducts().stream()
+                .map(product -> map(product, ItemDTO.class))
+                .collect(Collectors.toList());
+
+        destination.setItems(items);
+
+        return destination;
+    }
+
+}
---
+++ src/main/java/org/interlink/grouporder/misteram/controllers/OrderController.java
@@ -0,0 +1,85 @@
+package org.interlink.grouporder.misteram.controllers;
+
+
+import com.fasterxml.jackson.annotation.JsonView;
+import org.interlink.grouporder.core.data.DataStorage;
+import org.interlink.grouporder.core.entity.GroupOrder;
+import org.interlink.grouporder.core.entity.MemberOrder;
+import org.interlink.grouporder.core.entity.view.GroupOrderView;
+import org.interlink.grouporder.core.handler.ExceptionsHandler;
+import org.interlink.grouporder.core.utils.OrderCodeGenerator;
+import org.interlink.grouporder.misteram.MisterAmMapper;
+import org.interlink.grouporder.misteram.entity.FullOrderItemsDTO;
+import org.interlink.grouporder.misteram.entity.MemberOrderDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static org.interlink.grouporder.misteram.MisterAmMapper.map;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @PostMapping
+    @JsonView(GroupOrderView.Basic.class)
+    public GroupOrder createGroupOrder() {
+
+        String code = OrderCodeGenerator.generateCode();
+        GroupOrder groupOrder = new GroupOrder(code);
+        DataStorage.addGroupOrder(code, groupOrder);
+
+        ScheduledExecutorService schedule = new ScheduledThreadPoolExecutor(1);
+        schedule.schedule(() -> DataStorage.removeGroupOrder(code), 2, TimeUnit.HOURS);
+
+        return groupOrder;
+    }
+
+    @PostMapping("{code}/connect")
+    public ResponseEntity connectToGroupOrder(@PathVariable("code") String code) {
+        try {
+            if (DataStorage.isContains(code)) {
+                return ResponseEntity.ok("Success!");
+            } else{
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
+
+    @JsonView(GroupOrderView.Basic.class)
+    @PostMapping("/{code}/add-order")
+    public ResponseEntity addMemberToOrder(@PathVariable("code") String code, @RequestBody MemberOrderDTO newMemberOrderDTO) {
+        try {
+            MemberOrder memberOrder = map(newMemberOrderDTO, new MemberOrder());
+            DataStorage.getGroupOrder(code).addMemberToGroupOrder(memberOrder);
+            return ResponseEntity.ok("Success");
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
+
+    @GetMapping("/{code}/show-group-order")
+    public ResponseEntity showGroupOrder(@PathVariable("code") String code) {
+        try {
+            GroupOrder groupOrder = DataStorage.getGroupOrder(code);
+            return ResponseEntity.ok(groupOrder);
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
+
+    @GetMapping("/{code}/form-group-order")
+    public ResponseEntity formGroupOrder(@PathVariable("code") String code) {
+        try {
+            GroupOrder groupOrder = DataStorage.getGroupOrder(code);
+            return ResponseEntity.ok(map(groupOrder, new FullOrderItemsDTO()));
+        } catch (Exception e) {
+            return ExceptionsHandler.handleException(e);
+        }
+    }
+}
