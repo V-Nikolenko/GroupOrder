aff8ee25865fccfb7533e7c230715650d1224be9
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-01-29 15:18:35 +0200

    162_issues Step3: enhance: Added html for step 3

--- Extension/src/components/step3.vue
+++ Extension/src/components/step3.vue
@@ -1,25 +1,133 @@
 <template>
-    <div>{{step}}</div>
+    <div class="step3">
+        <h2
+                v-bind:class="['extension__step-heading', {'extension__step-heading_disabled': (!step.isDone && !step.isActive)}]">
+            Крок 3. Зібрати замовлення
+        </h2>
+
+        <div v-if="!step.isActive" class="step3__active-block">
+
+            <a href="#" v-on:click="showCheck = !showCheck">{{showCheckButton}}</a>
+            <div class="step3__container">
+
+                <button class="step3__btn">Зібрати</button>
+            </div>
+
+        </div>
+        <div v-else-if="step.isDone">
+
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    name: 'step1',
-    props: ['step'],
-    data() {
-        return {
-            
+    export default {
+        name: 'step3',
+        props: ['step'],
+
+        data() {
+            return {
+                showCheck: true,
+            }
+        },
+        computed: {
+            showCheckButton : function () {
+                return this.showCheck ? 'Показати' : 'Сховати';
+            }
+        },
+        methods: {
         }
-    },
-    computed: {
-
-    },
-    methods: {
-
     }
-}
 </script>
 
-<style lang="stylus" scoped>
+<style lang="scss" scoped>
+    $color1: silver;
+    $color2: #4b0082;
+    $color3: white;
+
+    .step3 {
+        display: flex;
+        flex-direction: column;
+
+        &__list {
+            padding: 0px;
+            margin: 0;
+            list-style-type: none;
+        }
+
+        &__container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            flex: 1;
+            justify-content: space-around
+        }
+
+        &__active-block {
+            display: flex;
+            flex-direction: column;
+            border: 1px solid silver;
+            border-top: none;
+            padding: 3px;
+            min-height: 250px;
+        }
+
+        &__delimeter {
+
+            &::after,
+            &::before {
+                content: '';
+                display: inline-block;
+                border-top: 2px solid silver;
+                width: 120px;
+                position: relative;
+                top: -2px;
+                transform: translate(0%, -50%);
+            }
+
+            &::after {
+                transform: translateX(15px)
+            }
+
+            &::before {
+                transform: translateX(-15px)
+            }
+        }
+
+        &__btn {
+            height: 50px;
+            color: $color2;
+            border: 1px solid $color2;
+            cursor: pointer;
+            border-radius: 10px;
+            background-color: $color3;
+            transition-duration: 0.1s;
+
+            &:hover {
+                color: $color3;
+                background-color: $color2;
+            }
+        }
+        &__btn,
+        &__input {
+            box-sizing: border-box;
+            width: 200px;
+            padding: 5px;
+
+        }
+
+        &__input {
+            min-height: 35px;
+            border-radius: 5px;
+            border: 1px solid $color1;
+
+            &:focus {
+                outline: none;
+                box-shadow: 0 0 1px 2px $color1;
+            }
+        }
+
+    }
+
 
 </style>
