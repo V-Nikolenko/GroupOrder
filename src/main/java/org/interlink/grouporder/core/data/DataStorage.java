be09160a67a6724e83c6cb40eb7fae7f1ee795fe
Andrew Maslyuk <tenza1999@gmail.com>
2020-01-31 12:25:58 +0200

    #198_issue DataStorage: refactor: refactored code generator and adding group orders

--- src/main/java/org/interlink/grouporder/core/data/DataStorage.java
+++ src/main/java/org/interlink/grouporder/core/data/DataStorage.java
@@ -3,7 +3,6 @@ package org.interlink.grouporder.core.data;
 import lombok.Getter;
 import lombok.Setter;
 import org.interlink.grouporder.core.entity.GroupOrder;
-import org.interlink.grouporder.core.exceptions.BadRequestException;
 import org.interlink.grouporder.core.utils.OrderCodeGenerator;
 
 import java.util.LinkedHashMap;
@@ -20,24 +19,19 @@ public class DataStorage {
     private static Map<String, GroupOrder> orders = new LinkedHashMap<>();
 
     public static String addGroupOrder() {
-        String code;
-
-        do {
-            code = OrderCodeGenerator.generateCode();
-        } while (DataStorage.isContains(code));
-
+        String code = generateUniqueCode();
         orders.put(code, new GroupOrder(code));
 
         return code;
     }
 
-    public static void removeGroupOrder(String code) {
-        if (code != null) {
-            orders.remove(code);
-        } else {
-            throw new BadRequestException("Key is invalid");
-        }
-    }
+//    public static void removeGroupOrder(String code) {
+//        if (code != null) {
+//            orders.remove(code);
+//        } else {
+//            throw new BadRequestException("Key is invalid");
+//        }
+//    }
 
     public static GroupOrder getGroupOrder(String code) {
         return Optional.ofNullable(orders.get(code)).orElseThrow(
@@ -46,5 +40,14 @@ public class DataStorage {
 
     public static boolean isContains(String code) {
         return orders.containsKey(code);
+    }
+
+    private static String generateUniqueCode() {
+        String code;
+        do {
+            code = OrderCodeGenerator.generateCode();
+        } while (DataStorage.isContains(code));
+
+        return code;
     }
 }
