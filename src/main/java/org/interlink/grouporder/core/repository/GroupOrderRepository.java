57bcbd0ab2ad16966cb1e37d5d54c308de1d967f
VadymSula <sulawados312000@gmail.com>
2020-02-04 16:01:23 +0200

    #223 feat: added query for getting GroupOrder

--- src/main/java/org/interlink/grouporder/core/repository/GroupOrderRepository.java
+++ src/main/java/org/interlink/grouporder/core/repository/GroupOrderRepository.java
@@ -9,10 +9,15 @@ import org.springframework.data.repository.query.Param;
 public interface GroupOrderRepository extends JpaRepository<GroupOrder, Integer> {
 
     @Modifying
-    @Query(value = "INSERT INTO group_order (group_order_code, restaurant_id, restaurant_name, restaurant_url) " +
-                    "VALUES  (:groupOrderCode, :restaurantId, :restaurantName, :restaurantUrl)", nativeQuery = true)
-    void addGroupOrder(@Param("groupOrderCode") String groupOrderCode,
+    @Query(value = "INSERT INTO group_order (code, restaurant_id, restaurant_name, restaurant_url) " +
+                   "VALUES  (:code, :restaurantId, :restaurantName, :restaurantUrl)", nativeQuery = true)
+    void addGroupOrder(@Param("groupOrderCode") String code,
                        @Param("restaurantId") String restaurantId,
                        @Param("restaurantName") String restaurantName,
                        @Param("restaurantUrl") String restaurantUrl);
+
+    @Query("SELECT g " +
+           "FROM group_order g" +
+            "WHERE g.code = :code")
+    GroupOrder getGroupOrder(@Param("code") String code);
 }
