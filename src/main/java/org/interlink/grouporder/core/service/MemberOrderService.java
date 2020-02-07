9b723808ad0db81c2cc95703d47a632995cf2d59
Vladislav Nicolenco <nicolenco2017@gmail.com>
2020-02-07 12:42:20 +0200

    check lock status method

--- src/main/java/org/interlink/grouporder/core/service/MemberOrderService.java
+++ src/main/java/org/interlink/grouporder/core/service/MemberOrderService.java
@@ -1,22 +1,12 @@
 package org.interlink.grouporder.core.service;
 
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import com.vladmihalcea.hibernate.type.json.JsonStringType;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
 import org.interlink.grouporder.core.entity.MemberOrder;
 import org.interlink.grouporder.core.repository.MemberOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Column;
 import java.util.List;
 
-@TypeDefs({
-        @TypeDef(name = "json", typeClass = JsonStringType.class),
-        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
-})
 @Service
 public class MemberOrderService {
 
